2b32341cc73debfabb9f5e95ec5c3e8851788050
Fernando <fernando.guastavino@gmail.com>
2022-05-24 09:05:31 -0300

    ejercicio if js

---
+++ script.js
@@ -0,0 +1,9 @@
+var temperatura = parseInt(prompt("Ingrese la temperatura de su casa."))
+
+if (temperatura < 0) {
+    alert("El arte es morirse de frío.")
+} else if( temperatura < 16) {
+    alert("Está fresco para pasear en jaula.")
+} else {
+    alert("Maldición va a ser un dia hermoso.")
+}
